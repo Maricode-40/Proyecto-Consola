1ae49c4d0f0f067329c04773181929d5b10d9705
Marissa <marissa.rico28@gmail.com>
2024-01-13 17:15:37 +0100

    actualizando musica y el html junto con el js funciones para cambiar imagenes

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+function change() {
+  var image = document.getElementById("changeImage");
+  image.src = "img/crash2.png";
+}
 
+theButton.onclick = function pictureChange() {
+  document.getElementById("changeImage").src = "img/crashbandicut3.jpeg";
+};
